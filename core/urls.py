e1ac7886d5420350d1874df492a8d2ed45f17ace
azamat-9933 <azamat.yakhyayeff@gmail.com>
2025-11-12 02:49:06 +0500

    added tg_bot.urls to core.urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf import settings
 from django.conf.urls.static import static
 
 from django.shortcuts import redirect
@@ -36,6 +35,9 @@ urlpatterns = [
 
     # swagger
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+    # tg_bot_app
+    path('tg_bot/', include('tg_bot.urls')),
 ]
 
 if settings.DEBUG:
